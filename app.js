2e2b9a9ad258f4cfc1d16c42fbad9c74a6ce6400
pawanveersinghshekhawat <pawanveersinghshekhawat44@gmail.com>
2024-12-15 12:13:51 +0530

    models and routers create

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express()
 
+const usersRouter = require("./routes/usersRouter")
+const ownersRouter = require("./routes/ownersRouter")
+const productsRouter = require("./routes/productsRouter")
 
-app.get("/" , (req,res)=>{
-    res.send("hey")
-})
+const path = require("path");
+const cookieParser = require("cookie-parser")
+const bodyParser = require('body-parser');
+const db = require("./config/mongoose-connection")
 
-app.listen(3000)
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(cookieParser());
+app.set("view engine","ejs");
+
+app.use("/users", usersRouter);
+app.use("/owners", ownersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
